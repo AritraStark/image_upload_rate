a4ed1522c58a4c1cf96c15cc9ccc1ea1f6992c82
AritraStark <58631915+AritraStark@users.noreply.github.com>
2020-07-25 11:13:53 +0530

    second responsive page added

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 var i=2;
-var toggler= document.querySelector(".legal");
+var toggler= document.querySelector(".toggle");
 toggler.addEventListener("click",function(){
   if(i%2==0)
   {
-    var a=document.querySelector(".w-nav-overlay");
+    var a=document.querySelector(".navbar2");
     a.classList.add("display");
     a.classList.remove("hidden");
 
   }
   else{
-    var a=document.querySelector(".w-nav-overlay");
+    var a=document.querySelector(".navbar2");
     a.classList.remove("display");
     a.classList.add("hidden");
   }
